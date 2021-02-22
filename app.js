ab43313e30ef097fb77c57b68e5543206cff6201
DxT9499 <dinh.tuyen.799@gmail.com>
2021-02-22 10:08:36 +0700

    upgrade drag&drop

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ function allowDrop(ev) {
     
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.effectAllowed = "copy";
 }
     
 function drop(ev) {
     ev.preventDefault();
+    var x = ev.clientX;
+    var y = ev.clientY;
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var copyimg = document.createElement("img");
+    var original = document.getElementById(data);
+    copyimg.src = original.src;
+    copyimg.style.position = "absolute";
+    copyimg.style.left = ev.clientX - ev.target.offsetLeft - copyimg.style.width/2 + "px";
+    copyimg.style.top = ev.clientY - ev.target.offsetTop - copyimg.style.height/2 + "px";
+    ev.target.appendChild(copyimg);
 }
