9be9320cc7689b5abe16bdebf3a35e7856ed24f0
DxT9499 <dinh.tuyen.799@gmail.com>
2021-02-23 20:27:58 +0700

    fix left vertical menu

--- app.js
+++ app.js
@@ -16,7 +16,27 @@ function drop(ev) {
     var original = document.getElementById(data);
     copyimg.src = original.src;
     copyimg.style.position = "absolute";
-    copyimg.style.left = ev.clientX - ev.target.offsetLeft - copyimg.style.width/2 + "px";
-    copyimg.style.top = ev.clientY - ev.target.offsetTop - copyimg.style.height/2 + "px";
+    copyimg.style.left = ev.clientX - ev.target.offsetLeft - (copyimg.style.width/2) + "px";
+    copyimg.style.top = ev.clientY - ev.target.offsetTop - (copyimg.style.height/2) + "px";
     ev.target.appendChild(copyimg);
+}
+
+function openList(evt, title) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    if(document.getElementById(title).style.display == "block") {
+        document.getElementById(title).style.display = "none";
+        evt.currentTarget.className -= "active";
+    }
+    else {
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(title).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
 }
